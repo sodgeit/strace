1f419b003f273d1eda78d6240275fcdb1326b58b
Christian Schneider <christian.schneider@sodgeit.de>
2024-06-28 14:32:21 +0200

    add functions necessary for the exechooks

--- src/exec_hooks.c
+++ src/exec_hooks.c
@@ -7,28 +7,724 @@
 
 #include "exec_hooks.h"
 
-void add_exec_hook(char* exec) {
-	fprintf(stderr, "%s\n", __func__);
+#include <limits.h>
+#include <linux/limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * There are four different blocks the exec_hooks will receive:
+ * They all contain at least an empty json
+ * - api block:
+ *  	- sent once to each exec_hook before tracing starts
+ *  	- called with at least these parameters:
+ *  		--api {"version": "<version of this api>", "syscalls": [<list of supported syscalls>]}
+ * - start block:
+ *  	- sent once to each exec_hook before tracing starts and after the api block
+ *  	- called with at least these parameters:
+ *  		--start {"call": "strace", "pid": <pid of strace>}
+ * - callback block:
+ *  	- sent to each exec_hook for each syscall
+ *  	- called with at least these parameters:
+ *  		--callback <json with data from the syscall>
+ *  			-> for the returnvalue of clone the following json data will be sent:
+ *  				{"syscall": "clone", "pid": <pid returned by clone>, 
+ *  					"ppid": <the parent pid to the to the pid returned by clone>}
+ *  			-> for the execve syscall the following json data will be sent:
+ *  				{"syscall": "execve", "pids": <pid of the calling process of the execve>, 
+ *  					"ppid": <the parent pid to the process calling execve>, 
+ *  					"exec_path": "<the execpath used in execve>", 
+ *  					"current_directory": "<the current directory of the process calling execve>", 
+ *  					"argv": ["<all arguments present in execve>"]}
+ *  			-> other syscalls and returnvalues not implemented
+ * - end block:
+ *  	- sent once to each exec_hook after tracing ended
+ *  	- called with at least these parameters:
+ *  		--end {}
+ *
+ * Additionally to the parameters mentioned above own options can be passed to the exec_hooks by using the 
+ * '--exec-hook-param' option of strace. 
+ */
+
+enum callback_type {
+	CALLBACK_API = 0,
+	CALLBACK_START,
+	CALLBACK_SYSCALL,
+	CALLBACK_END,
+};
+
+enum syscall_type {
+	SYSCALL_EXECVE = 0,
+	SYSCALL_CLONE,
+};
+
+struct exec_hook_param {
+	struct exec_hook_param *next;
+	char *param;
+};
+
+struct exec_hook {
+	struct exec_hook *next;
+	char exec_path[PATH_MAX];
+	int number_of_params;
+	struct exec_hook_param *params;
+};
+
+struct syscall_info {
+	enum syscall_type type;
+	pid_t pid;
+	pid_t ppid;
+};
+
+struct execve_info {
+	struct syscall_info syscall;
+	char exec_path[PATH_MAX];
+	char current_directory[PATH_MAX];
+	int argv_length;
+	char **argv;
+};
+
+void remove_exec_hooks(void);
+void remove_all_params_from_exec_hooks(void);
+
+pid_t get_ppid(pid_t pid);
+int get_current_directory(pid_t pid, char *current_directory);
+int get_string(struct tcb *tcp, const kernel_ulong_t addr, char *buffer, int buff_size);
+char** get_argv(struct tcb *tcp, kernel_ulong_t addr, int *length);
+
+int append_to_buffer_data_escaped(char **buffer, int *current_offset, int *buffer_size, char *data);
+int append_to_buffer(char **buffer, int *current_offset, int *buffer_size, const char *data);
+int append_string_to_json(char **buffer, int *current_offset, int *buffer_size, const char *field_name, char *data);
+int append_string_array_to_json(char **buffer, int *current_offset, int *buffer_size, const char *field_name, char **data, int data_length);
+const char* get_syscall_name(enum syscall_type syscall);
+const char* get_callback_type_option(enum callback_type syscall);
+int append_execve_info_to_json(struct execve_info *syscall_info, char **buffer, int *current_offset, int *buffer_size);
+int append_additional_syscall_info_to_json(struct syscall_info *syscall_info, char **buffer, int *current_offset, int *buffer_size);
+char* build_json(struct syscall_info *syscall_info);
+void send_data_to_exec_hooks(enum callback_type type, char *data);
+void call_exec_hooks(struct syscall_info *info);
+
+static const char* callback_options[] = {
+	[CALLBACK_API] = "--api",
+	[CALLBACK_START] = "--start",
+	[CALLBACK_SYSCALL] = "--callback",
+	[CALLBACK_END] = "--end"
+};
+
+struct exec_hook *active_exec_hooks = NULL;
+
+static const char api_version[] = "1.0.0";
+
+void add_exec_hook(char *exec) {
+	struct exec_hook *new_exec_hook = malloc(sizeof(struct exec_hook));
+	if (new_exec_hook == NULL) {
+		exit(1);
+	}
+
+	realpath(exec, new_exec_hook->exec_path);
+	new_exec_hook->next = NULL;
+	new_exec_hook->number_of_params = 0;
+	new_exec_hook->params = NULL;
+
+	if (active_exec_hooks == NULL) {
+		active_exec_hooks = new_exec_hook;
+		return;
+	}
+
+	struct exec_hook *insert_pos = active_exec_hooks;
+	while (insert_pos->next != NULL) {
+		insert_pos = insert_pos->next;
+	}
+	insert_pos->next = new_exec_hook;
 }
 
-void add_param_to_current_exec_hook(char* param) {
-	fprintf(stderr, "%s\n", __func__);
+void add_param_to_current_exec_hook(char *param) {
+	if (active_exec_hooks == NULL) {
+		fprintf(stderr, "Can't add a parameter, when no hook has been added. Ignoring parameter...");
+		return;
+	}
+
+	struct exec_hook_param *new_exec_hook_param = malloc(sizeof(struct exec_hook_param));
+
+	if (new_exec_hook_param == NULL) {
+		exit(1);
+	}
+
+	new_exec_hook_param->next = NULL;
+	new_exec_hook_param->param = param;
+
+	// get last added hook
+	struct exec_hook *current_hook = active_exec_hooks;
+	while (current_hook->next != NULL) {
+		current_hook = current_hook->next;
+	}
+
+	// insert parameter into parameter linked list
+	if (current_hook->params == NULL) {
+		current_hook->params = new_exec_hook_param;
+	} else {
+		struct exec_hook_param *insert_pos = current_hook->params;
+
+		while (insert_pos->next != NULL) {
+			insert_pos = insert_pos->next;
+		}
+
+		insert_pos->next = new_exec_hook_param;
+	}
+
+	current_hook->number_of_params++;
+}
+
+void remove_exec_hooks(void) {
+	struct exec_hook *current_exec_hook = active_exec_hooks;
+
+	while (current_exec_hook != NULL) {
+		struct exec_hook *delete_hook = current_exec_hook;
+		current_exec_hook = current_exec_hook->next;
+		free(delete_hook);
+	}
+}
+
+void remove_all_params_from_exec_hooks(void) {
+	struct exec_hook *current_exec_hook = active_exec_hooks;
+
+	while (current_exec_hook != NULL) {
+		struct exec_hook_param *current_param = current_exec_hook->params;
+		while (current_param != NULL) {
+			struct exec_hook_param *delete_param = current_param;
+			current_param = current_param->next;
+			free(delete_param);
+		}
+		current_exec_hook = current_exec_hook->next;
+	}
+}
+
+void send_data_to_exec_hooks(enum callback_type type, char *data) {
+	const char *callback_option = get_callback_type_option(type);
+
+	if (callback_option == NULL) {
+		return;
+	}
+
+	char option[100];
+	strcpy(option, callback_option);
+
+	struct exec_hook *current_hook = active_exec_hooks;
+
+	while (current_hook != NULL) {
+		pid_t pid = fork();
+
+		if (pid < 0) {
+			return;
+		}
+
+		if (pid == 0) {
+			// the four fixed arguments are:
+			// - exec path
+			// - callback option
+			// - data of the callback
+			// - ending NULL
+			int number_of_arguments = 4 + current_hook->number_of_params;
+			char **arguments = malloc(number_of_arguments * sizeof(char*));
+
+			int i = 0;
+			struct exec_hook_param *current_param = current_hook->params;
+			
+			arguments[i++] = current_hook->exec_path;
+			while (current_param != NULL) {
+				arguments[i++] = current_param->param;
+				current_param = current_param->next;
+			}
+			arguments[i++] = option;
+			arguments[i++] = data;
+			arguments[i++] = NULL;
+
+			execve(current_hook->exec_path, arguments, NULL);
+			// execve() does not return on success 
+			// -> terminate child process when execve() fails
+			free(arguments);
+			exit(1);
+		}
+
+		waitpid(pid, NULL, 0);
+
+		current_hook = current_hook->next;
+	}
 }
 
 void init_exec_hooks(void) {
-	fprintf(stderr, "%s\n", __func__);
+	static char api_json[100];
+	snprintf(api_json, sizeof(api_json) - 1, "{\"version\":\"%s\",\"syscalls\":[\"execve\",\"clone\"]}", api_version);
+
+	send_data_to_exec_hooks(CALLBACK_API, api_json);
+
+	char start_json[100];
+	snprintf(start_json, sizeof(start_json) - 1, "{\"call\":\"strace\",\"pid\":%d}", getpid());
+
+	send_data_to_exec_hooks(CALLBACK_START, start_json);
 }
 
 void cleanup_exec_hooks(void) {
-	fprintf(stderr, "%s\n", __func__);
+	static char cleanup_json[] = "{}";
+
+	send_data_to_exec_hooks(CALLBACK_END, cleanup_json);
+
+	remove_all_params_from_exec_hooks();
+	remove_exec_hooks();
+}
+
+const char* get_syscall_name(enum syscall_type syscall) {
+	switch (syscall) {
+		case SYSCALL_EXECVE:
+			return "execve";
+		case SYSCALL_CLONE: 
+			return "clone";
+		default:
+			return "unknown";
+	}
+}
+
+const char* get_callback_type_option(enum callback_type type) {
+	switch (type) {
+		case CALLBACK_API:
+		case CALLBACK_START:
+		case CALLBACK_SYSCALL:
+		case CALLBACK_END:
+			return callback_options[type];
+		default:
+			return NULL;
+	}
+}
+
+/**
+ * Returns the written bytes in success, else 0
+ */
+int append_to_buffer_data_escaped(char **buffer, int *current_offset, int *buffer_size, char *data) {
+	char *read_ptr = data;
+	char *write_ptr = *buffer + *current_offset * sizeof(char);
+	int prev_offset = *current_offset;
+
+	while (*read_ptr != '\0') {
+		if (*current_offset + 4 >= *buffer_size) {
+			int new_buffer_size = *buffer_size * 2;
+			char *new_buffer = realloc(*buffer, new_buffer_size * sizeof(char));
+			if (new_buffer == NULL) {
+				return 0;
+			}
+			*buffer = new_buffer;
+			*buffer_size = new_buffer_size;
+			write_ptr = *buffer + *current_offset * sizeof(char);
+		}
+
+		switch (*read_ptr) {
+			case '\n':
+				*write_ptr++ = '\\';
+				*write_ptr++ = 'n';
+				*current_offset += 2;
+				break;
+			case '\\':
+				*write_ptr++ = '\\';
+				*write_ptr++ = '\\';
+				*current_offset += 2;
+				break;
+			case '\t':
+				*write_ptr++ = '\\';
+				*write_ptr++ = 't';
+				*current_offset += 2;
+				break;
+			case '\r':
+				*write_ptr++ = '\\';
+				*write_ptr++ = 'r';
+				*current_offset += 2;
+				break;
+			case '\f':
+				*write_ptr++ = '\\';
+				*write_ptr++ = 'f';
+				*current_offset += 2;
+				break;
+			case '\"':
+				*write_ptr++ = '\\';
+				*write_ptr++ = '"';
+				*current_offset += 2;
+				break;
+			 case '\b':
+				*write_ptr++ = '\\';
+				*write_ptr++ = 'b';
+				*current_offset += 2;
+				break;
+			default:
+				*write_ptr++ = *read_ptr;
+				*current_offset += 1;
+		}
+		read_ptr++;
+	}
+	*write_ptr++ = '\0';
+	return *current_offset - prev_offset;
+}
+
+/**
+ * Returns the written bytes in success, else 0
+ */
+int append_to_buffer(char **buffer, int *current_offset, int *buffer_size, const char *data) {
+	int data_size = strlen(data);
+
+	if (data_size + *current_offset >= *buffer_size) {
+		int new_buffer_size = *buffer_size * 2;
+		char *new_buffer = realloc(*buffer, new_buffer_size * sizeof(char));
+		if (new_buffer == NULL) {
+			return 0;
+		}
+		*buffer = new_buffer;
+		*buffer_size = new_buffer_size;
+	}
+	
+	int written_bytes = sprintf(&((*buffer)[*current_offset]), "%s", data);
+	if (written_bytes < 0) {
+		return 0;
+	}
+	*current_offset += written_bytes;
+	return written_bytes + 1;
+}
+
+int append_string_to_json(char **buffer, int *current_offset, int *buffer_size, const char *field_name, char *string) {
+	if (!append_to_buffer(buffer, current_offset, buffer_size, ",\"")) {
+		return -1;
+	}
+	if (!append_to_buffer(buffer, current_offset, buffer_size, field_name)) {
+		return -1;
+	}
+	if (!append_to_buffer(buffer, current_offset, buffer_size, "\":\"")) {
+		return -1;
+	}
+	if (!append_to_buffer_data_escaped(buffer, current_offset, buffer_size, string)) {
+		return -1;
+	}
+	if (!append_to_buffer(buffer, current_offset, buffer_size, "\"")) {
+		return -1;
+	}
+	return 0;
+}
+
+int append_string_array_to_json(char **buffer, int *current_offset, int *buffer_size, const char *field_name, char **data, int data_length) {
+	if (data_length < 0) {
+		return -1;
+	}
+
+	if (!append_to_buffer(buffer, current_offset, buffer_size, ",\"")) {
+		return -1;
+	}
+	if (!append_to_buffer(buffer, current_offset, buffer_size, field_name)) {
+		return -1;
+	}
+
+	if (data_length == 0) {
+		if (!append_to_buffer(buffer, current_offset, buffer_size, "\":[]")) {
+			return -1;
+		}
+		return 0;
+	}
+
+	if (!append_to_buffer(buffer, current_offset, buffer_size, "\":[\"")) {
+		return -1;
+	}
+
+	if (!append_to_buffer_data_escaped(buffer, current_offset, buffer_size, data[0])) {
+		return -1;
+	}
+
+	for (int i = 1; i < data_length; i++) {
+		if (!append_to_buffer(buffer, current_offset, buffer_size, "\",\"")) {
+			return -1;
+		}
+
+		if (!append_to_buffer_data_escaped(buffer, current_offset, buffer_size, data[i])) {
+			return -1;
+		}
+	}
+
+	if (!append_to_buffer(buffer, current_offset, buffer_size, "\"]")) {
+		return -1;
+	}
+	return 0;
+}
+
+int append_execve_info_to_json(struct execve_info *syscall_info, char **buffer, int *current_offset, int *buffer_size) {
+	int success = append_string_to_json(buffer, current_offset, buffer_size, "exec_path", syscall_info->exec_path);
+	if (success < 0) {
+		return -1;
+	}
+	success = append_string_to_json(buffer, current_offset, buffer_size, "current_directory", syscall_info->current_directory);
+	if (success < 0) {
+		return -1;
+	}
+	success = append_string_array_to_json(buffer, current_offset, buffer_size, "argv", syscall_info->argv, syscall_info->argv_length);
+	if (success < 0) {
+		return -1;
+	}
+	return 0;
+}
+
+int append_additional_syscall_info_to_json(struct syscall_info *syscall_info, char **buffer, int *current_offset, int *buffer_size) {
+	switch (syscall_info->type) {
+		case SYSCALL_EXECVE:
+			return append_execve_info_to_json((struct execve_info*)syscall_info, buffer, current_offset, buffer_size);
+		default:
+			return 0;
+	}
+}
+
+/**
+ * the json returned has to be freed
+ */
+char *build_json(struct syscall_info *syscall_info) {
+	int buffer_size = PATH_MAX;
+	int current_offset = 0;
+	char *json_buffer = malloc(buffer_size * sizeof(char));
+
+	if (json_buffer == NULL) {
+		return NULL;
+	}
+
+	current_offset = snprintf(json_buffer, buffer_size - 1, "{\"syscall\":\"%s\",\"pid\":%d,\"ppid\":%d", get_syscall_name(syscall_info->type), syscall_info->pid, syscall_info->ppid);
+
+	int success = append_additional_syscall_info_to_json(syscall_info, &json_buffer, &current_offset, &buffer_size);
+
+	if (success < 0) {
+		free(json_buffer);
+		return NULL;
+	}
+
+	if (!append_to_buffer(&json_buffer, &current_offset, &buffer_size, "}")) {
+		free(json_buffer);
+		return NULL;
+	}
+
+	return json_buffer;
+}
+
+void call_exec_hooks(struct syscall_info *syscall_info) {
+	char *json = build_json(syscall_info);
+
+	if (json == NULL) {
+		return;
+	}
+	
+	send_data_to_exec_hooks(CALLBACK_SYSCALL, json);
+
+	free(json);
+}
+
+pid_t get_ppid(pid_t pid) {
+	static const int path_size = 50;
+	static const int buffer_size = 4096;
+
+	int ppid;
+	char process_information[buffer_size];
+	char process_information_path[path_size];
+	FILE *fp;
+
+	snprintf(process_information_path, path_size, "/proc/%u/status", pid);
+	fp = fopen(process_information_path, "r");
+	if (fp != NULL) {
+		size_t ret = fread(process_information, sizeof(char), buffer_size - 1, fp);
+		if (!ret) {
+			return -1;
+		} else {
+			process_information[ret++] = '\0';
+		}
+	}
+	fclose(fp);
+	char *ppid_loc = strstr(process_information, "\nPPid:");
+	if (ppid_loc) {
+		int ret = sscanf(ppid_loc, "\nPPid:%d", &ppid);
+		if (!ret || ret == EOF) {
+			return -1;
+		}
+		return ppid;
+	} else {
+		return -1;
+	}
+}
+
+int get_current_directory(pid_t pid, char *current_directory) {
+	static const int buffer_size = 300;
+	char buffer[buffer_size];
+
+	if (!pid) {
+		return 0;
+	}
+
+	if (snprintf(buffer, buffer_size - 1, "/proc/%d/cwd", pid) < (int)sizeof(buffer)) {
+		ssize_t length = readlink(buffer, current_directory, PATH_MAX - 1);
+		if (length < PATH_MAX - 1) {
+			current_directory[length] = '\0';
+			return length;
+		}
+	}
+	return 0;
+}
+
+int get_string(struct tcb *tcp, const kernel_ulong_t addr, char *buffer, int buff_size) {
+	if (!tcp || !addr) {
+		return -1;
+	}
+
+	if (!buffer || buff_size <= 0) {
+		return -1;
+	}
+
+	return umovestr(tcp, addr, buff_size, buffer);
+}
+
+/**
+ * the logic used here is mostly from the printargv(...) function in the file execve.c
+ * 
+ * the returned char** pointer and char* pointers inside have to be freed
+ */
+char** get_argv(struct tcb *tcp, kernel_ulong_t addr, int *length) {
+	if (!tcp || !addr) {
+		goto exit_error;
+	}
+
+	if (!length) {
+		goto exit_error;
+	}
+
+	char **argv;
+	int capacity = 4;
+
+	argv = malloc(sizeof(char*) * capacity);
+
+	if (argv == NULL) {
+		goto exit_error;
+	}
+
+	const unsigned int wordsize = current_wordsize;
+	kernel_ulong_t prev_addr = 0;
+	unsigned int n = 0;
+	*length = 0;
+
+	for (;;prev_addr = addr, addr += wordsize, ++n) {
+		union {
+			unsigned int w32;
+			kernel_ulong_t wl;
+			char data[sizeof(kernel_ulong_t)];
+		} cp;
+
+		if (addr < prev_addr || umoven(tcp, addr, wordsize, cp.data)) {
+			if (n == 0) {
+				goto exit_error_free_argv;
+			}
+			break;
+		}
+
+		const kernel_ulong_t word = (wordsize == sizeof(cp.w32)) ? (kernel_ulong_t) cp.w32 : cp.wl;
+		
+		if (word == 0) {
+			break;
+		}
+
+		char *argument = NULL;
+		int argument_length = PATH_MAX;
+
+		int result = 0;
+
+		while (result == 0) {
+			char *new_argument = realloc(argument, sizeof(char) * argument_length);
+
+			if (new_argument == NULL) {
+				if (argument) {
+					free(argument);
+				}
+				goto exit_error_free_argv;
+			}
+			argument = new_argument;
+
+			result = get_string(tcp, word, argument, argument_length);
+
+			if (result < 0) {
+				free(argument);
+				goto exit_error_free_argv;
+			}
+			if (result > 0) {
+				break;
+			}
+
+			argument_length *= 2;
+		}
+
+		if (*length + 1 > capacity) {
+			capacity *= 2;
+			char **new_argv = realloc(argv, sizeof(char*) * capacity);
+			if (new_argv == NULL) {
+				free(argument);
+				goto exit_error_free_argv;
+			}
+			argv = new_argv;
+		}
+
+		argv[*length] = argument;
+		*length += 1;
+	}
+
+	return argv;
+
+exit_error_free_argv:
+	for (int i = 0; i < *length; i++) {
+		free(argv[i]);
+	}
+	free(argv);
+
+exit_error:
+	*length = -1;
+	return NULL;
 }
 
 // exec-hooks
-
 void clone_returnval_exec_hook(kernel_long_t pid) {
-	fprintf(stderr, "%s\n", __func__);
+	if (!active_exec_hooks) {
+		return;
+	}
+
+	struct syscall_info clone_info;
+	clone_info.type = SYSCALL_CLONE;
+	clone_info.pid = get_proc_pid(pid);
+	clone_info.ppid = get_ppid(clone_info.pid);
+
+	if (clone_info.ppid < 0) {
+		return;
+	}
+
+	call_exec_hooks(&clone_info);
 }
 
 void execve_exec_hook(struct tcb *tcp, const unsigned int index) {
-	fprintf(stderr, "%s\n", __func__);
+	if (!active_exec_hooks) {
+		return;
+	}
+
+	if (!tcp->pid) {
+		return;
+	}
+
+	struct execve_info syscall_info;
+	syscall_info.syscall.type = SYSCALL_EXECVE;
+	syscall_info.syscall.pid = get_proc_pid(tcp->pid);
+	syscall_info.syscall.ppid = get_ppid(syscall_info.syscall.pid);
+	int current_directory_length = get_current_directory(syscall_info.syscall.pid, syscall_info.current_directory);
+	int success = get_string(tcp, tcp->u_arg[index], syscall_info.exec_path, sizeof(syscall_info.exec_path));
+	syscall_info.argv = get_argv(tcp, tcp->u_arg[index + 1], &syscall_info.argv_length);
+
+	if (current_directory_length == 0 || success < 0 || syscall_info.argv_length < 0 || syscall_info.syscall.ppid < 0) {
+		return;
+	}
+
+	call_exec_hooks((struct syscall_info*)&syscall_info);
+
+	for (int i = 0; i < syscall_info.argv_length; i++) {
+		free(syscall_info.argv[i]);
+	}
+	free(syscall_info.argv);
 }
